511598d9830614a4e8565617c3e5ba43dae475ae
anand <manchopsy@gmail.com>
2020-01-04 15:21:04 +0530

    anandgit

--- src/main/java/com/AutomationPractice/HomepageObjectsAP.java
+++ src/main/java/com/AutomationPractice/HomepageObjectsAP.java
@@ -10,7 +10,7 @@ public class HomepageObjectsAP extends BasePage1{
 	private WebElement Women;
 	
 	@FindBy(xpath="(//a[@title='Dresses'])[2]")
-	private WebElement dress;
+	private WebElement dress3232;
 	
 //	@FindBy(xpath="(//li[@class]//a[@title='Dresses']")
 //	private WebElement dress;
